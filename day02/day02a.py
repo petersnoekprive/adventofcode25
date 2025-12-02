c770f8f40e6e46dd285ff2e0e84c317276c1028e
petersnoekprive <46825069+petersnoekprive@users.noreply.github.com>
2025-12-02 18:28:22 +0100

    solved day02 by summing numbers with repeating digits

--- day02/day02a.py
+++ day02/day02a.py
@@ -4,12 +4,40 @@ import os
 import sys
 
 # read input file into "lines"
-input_file = os.path.join(sys.path[0], "example02.txt")
-# input_file = os.path.join(sys.path[0], "input02.txt")
+# input_file = os.path.join(sys.path[0], "example02.txt")
+input_file = os.path.join(sys.path[0], "input02.txt")
 with open(input_file) as f:
     lines = f.read().splitlines()
 
+
+def sum_false_ids_in_range(range_str):
+    count = 0
+    start, end = map(int, range_str.split("-"))
+    for i in range(start, end + 1):
+        # check if i has a even number of digits, like 2, 4, 6, 8 etc.
+        if len(str(i)) % 2 == 0:
+            # cast i to string and split into two halves, and check if both halves are the same
+            # e.g. 1221 -> "12" and "21" -> not the same
+            # e.g. 11 -> "1" and "1" -> the same
+            str_i = str(i)
+            half = len(str_i) // 2
+            if str_i[:half] == str_i[half:]:
+                count += i
+                print("  Found false ID: ", i)
+    return count
+
+parts = []
 for line in lines:
     print("Line: ", line)
+    parts.append(line.split(","))
+
 print("---------------")
-print("ANSWER: ")
+print("PARTS: ")
+total = 0
+for part in parts:
+    for item in part:
+        print(item)
+        total += sum_false_ids_in_range(item)
+
+print("---------------")
+print("Total count of false IDs: ", total)
--- day02/day02b.py
+++ day02/day02b.py
@@ -2,9 +2,39 @@
 
 import os
 import sys
+import re
 
 # read input file into "lines"
-input_file = os.path.join(sys.path[0], "example02.txt")
-# input_file = os.path.join(sys.path[0], "input02.txt")
+# input_file = os.path.join(sys.path[0], "example02.txt")
+input_file = os.path.join(sys.path[0], "input02.txt")
 with open(input_file) as f:
     lines = f.read().splitlines()
+
+
+def is_repeating(s):
+    return bool(re.match(r"^(.+)\1+$", s))
+
+def count_false_ids_in_range(range_str):
+    count = 0
+    start, end = map(int, range_str.split("-"))
+    for i in range(start, end + 1):
+        if is_repeating(str(i)):
+            count += i
+            print("  Found false ID: ", i)
+    return count
+
+parts = []
+for line in lines:
+    print("Line: ", line)
+    parts.append(line.split(","))
+
+print("---------------")
+print("PARTS: ")
+total = 0
+for part in parts:
+    for item in part:
+        print(item)
+        total += count_false_ids_in_range(item)
+
+print("---------------")
+print("Total count of false IDs: ", total)
